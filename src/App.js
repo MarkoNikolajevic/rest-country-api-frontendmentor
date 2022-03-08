0bcf7662fb93e30f651c2ba0f2018cceb4c740a9
Marko Nikolajevic <mrknikolajevic@gmail.com>
2022-03-08 16:00:46 +0100

    update api endpoint

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get('https://restcountries.eu/rest/v2/all/')
+      .get('https://restcountries.com/v2/all')
       .then((response) => setCountries(response.data))
       .catch((error) => console.log(error));
   }, []);
