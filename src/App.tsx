102c7d1b7f3731ba974ba03cb2fe301384c6f4ee
Marko Nikolajevic <mrknikolajevic@gmail.com>
2024-02-27 14:28:29 +0100

    trigger pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 function App() {
   return (
-    <>
-      <main className='bg-dark flex h-dvh w-screen items-center justify-center text-white'>
-        <h1 className='text-5xl'>Re-working in progress</h1>
-      </main>
-    </>
+    <main className='bg-dark flex h-dvh w-screen items-center justify-center text-white'>
+      <h1 className='text-5xl'>Re-working in progress</h1>
+    </main>
   );
 }
 
